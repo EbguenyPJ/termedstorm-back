6190fbc1d93fc24d8d02c236c2798e2e72dca4c6
florludovico <florencia.ludovico@gmail.com>
2025-06-13 09:01:01 -0300

    resolve: conflictos de merge con origin/steven

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeorm from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosModule } from './modules/todos/todos.module';
+import { MembershipStatusModule } from './catalogues/userMembershipStatus/membership-status.module';
+import { SubCategoryModule } from './catalogues/subCategory/sub-category.module';
+import { PaymentMethodModule } from './catalogues/paymentMethod/payment-method.module';
+import { GenderModule } from './catalogues/gender/gender.module';
+import { CategoryModule } from './catalogues/category/category.module';
+import { CancelReasonModule } from './catalogues/cancelReason/cancel-reason.module';
+import { BrandModule } from './catalogues/brand/brand.module';
+import { ProductModule } from './modules/products/product.module';
+import { CsvUploadModule } from './modules/csvUpload/csv-upload.module';
+import { ProductVariant } from './modules/productsVariant/entities/product-variant.entity';
+import { ProductModification } from './modules/productModification/entities/product-modification.entity';
 
 @Module({
   imports: [
@@ -15,6 +26,17 @@ import { TodosModule } from './modules/todos/todos.module';
       useFactory: (config: ConfigService) => config.get('typeorm')!,
     }),
     TodosModule,
+    MembershipStatusModule,
+    SubCategoryModule,
+    PaymentMethodModule,
+    GenderModule,
+    CategoryModule,
+    CancelReasonModule,
+    BrandModule,
+    ProductModule,
+    ProductVariant,
+    ProductModification,
+    CsvUploadModule,
   ],
   controllers: [],
   providers: [],
