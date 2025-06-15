cd8e4efefb8e00c312b3b8229db8eb2d2494dd20
Stevecardonam <steve.cardm@gmail.com>
2025-06-14 23:53:23 -0500

    update: product.service

--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -121,18 +121,18 @@ export class ProductService {
     return { message: `Product with ID ${id} deleted successfully` };
   }
 
-    //  async findManyVariantsByIds(ids: string[]): Promise<ProductVariant[]> {
-    //   if (!ids || ids.length === 0) {
-    //     return [];
-    //   }
-  
-    //   return this.variantRepository.find({
-    //     where: {
-    //       id: In(ids),
-    //     },
-    //     relations: {
-    //       product: true,
-    //     },
-    //   });
-    // }
+  async findManyVariantsByIds(ids: string[]): Promise<ProductVariant[]> {
+    if (!ids || ids.length === 0) {
+      return [];
+    }
+
+    return this.variantRepository.find({
+      where: {
+        id: In(ids),
+      },
+      relations: {
+        product: true,
+      },
+    });
+  }
 }
