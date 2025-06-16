e3bd0b4d7b27b62e44a70c1a3a97aedeaa0d2e79
IWeppler <ignacionweppler@gmail.com>
2025-06-16 01:26:51 -0300

    ft: secure and sameSite

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost/:4000',
+    origin: 'http://localhost:4000',
     credentials: true, 
   });
   app.use(loggerGlobal);
