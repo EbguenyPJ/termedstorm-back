5136155813d90fdf86a22378baeae59a1661b2e7
Pia <piaborgoglio80@gmail.com>
2025-06-26 13:26:49 -0300

    Guardo avances antes de cambiar de rama

--- src/modules/shipments/shipments.controller.ts
+++ src/modules/shipments/shipments.controller.ts
@@ -10,6 +10,8 @@ import {
   UseInterceptors,
   UploadedFile,
   Res,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ShipmentsService } from './shipments.service';
 import { UpdateShipmentDto } from './dtos/update-shipment.dto';
@@ -17,6 +19,11 @@ import { CreateShipmentDto } from './dtos/create-shipment.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import { ShipmentsCsvService } from './csv/shipments-csv.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
 @Controller('shipments')
 export class ShipmentsController {
   constructor(
@@ -52,15 +59,145 @@ export class ShipmentsController {
     return this.shipmentsService.remove(id);
   }
 
-  @Get('csv/download')
-  async downloadCsv(@Res() res: Response) {
-    const filePath = await this.csvService.createCsvFromDatabase('embarques');
-    res.download(filePath);
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @Get('csv/from-db')
+  async downloadCsvFromDb(
+    @Query('filename') filename: string,
+    @Query('code') code: string,
+    @Query('from') from: string,
+    @Query('to') to: string,
+    @Res() res: Response
+  ) {
+    const filePath = await this.csvService.createCsvFromDatabase(
+      filename || 'embarques',
+      code,
+      from,
+      to,
+    );
+    res.download(filePath, `${filename || 'embarques'}.csv`);
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
   @Post('csv/upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './temp',
+        filename: (req, file, cb) => {
+          const uniqueName = `${Date.now()}${extname(file.originalname)}`;
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
   async uploadCsv(@UploadedFile() file: Express.Multer.File) {
     return this.csvService.loadCsvToDatabase(file);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// import {
+//   Body,
+//   Controller,
+//   Get,
+//   Post,
+//   Param,
+//   Put,
+//   Delete,
+//   ParseIntPipe,
+//   UseInterceptors,
+//   UploadedFile,
+//   Res,
+//   Query,
+// } from '@nestjs/common';
+// import { ShipmentsService } from './shipments.service';
+// import { UpdateShipmentDto } from './dtos/update-shipment.dto';
+// import { CreateShipmentDto } from './dtos/create-shipment.dto';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { Response } from 'express';
+// import { ShipmentsCsvService } from './csv/shipments-csv.service';
+// import { diskStorage } from 'multer';
+// import { extname } from 'path';
+// import { UseGuards } from '@nestjs/common';
+// import { ApiBearerAuth } from '@nestjs/swagger'; 
+// import { AuthGuard } from '../auth/guards/auth.guard';
+// @Controller('shipments')
+// export class ShipmentsController {
+//   constructor(
+//     private readonly shipmentsService: ShipmentsService,
+//     private readonly csvService: ShipmentsCsvService
+//   ) {}
+
+//   @Post()
+//   create(@Body() dto: CreateShipmentDto) {
+//     return this.shipmentsService.create(dto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.shipmentsService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id', ParseIntPipe) id: number) {
+//     return this.shipmentsService.findOne(id);
+//   }
+
+//   @Put(':id')
+//   update(
+//     @Param('id', ParseIntPipe) id: number,
+//     @Body() dto: UpdateShipmentDto,
+//   ) {
+//     return this.shipmentsService.update(id, dto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id', ParseIntPipe) id: number) {
+//     return this.shipmentsService.remove(id);
+//   }
+
+//  @Get('csv/from-db')
+// async downloadCsvFromDb(
+//   @Query('filename') filename: string,
+//   @Query('code') code: string,
+//   @Query('from') from: string,
+//   @Query('to') to: string,
+//   @Res() res: Response
+// ) {
+//   const filePath = await this.csvService.createCsvFromDatabase(filename || 'embarques', code, from, to);
+//   res.download(filePath, `${filename || 'embarques'}.csv`);
+// }
+
+
+
+// @Post('csv/upload')
+// @UseInterceptors(
+//   FileInterceptor('file', {
+//     storage: diskStorage({
+//       destination: './temp',
+//       filename: (req, file, cb) => {
+//         const uniqueName = `${Date.now()}${extname(file.originalname)}`;
+//         cb(null, uniqueName);
+//       },
+//     }),
+//   }),
+// )
+// async uploadCsv(
+//   @UploadedFile() file: Express.Multer.File,
+// ) {
+//   return this.csvService.loadCsvToDatabase(file);
+// }
+
+
+// }
